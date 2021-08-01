ae2aa6cf82723b2fff0c597983b26a0aec251af9
Justin Turner <jmturner638@gmail.com>
2021-08-01 03:13:15 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,11 @@
 def main():
     #write your code below this line
+    pass
+
+def print_numbers_in_range(numbers, lower_limit, upper_limit):
+    for number in numbers:
+        if number >= lower_limit and number <= upper_limit:
+            print(number)
 
 if __name__ == '__main__':
     main()
