ec90666e5f6faba82631fa57045501e8926650ec
Justin Turner <jmturner638@gmail.com>
2021-08-01 03:31:02 -0500

    Third Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,11 +1,10 @@
 def main():
     #write your code below this line
     numbers = [1,3,10,-1,5,0]
-
-    print("The numbers in the range [0, 5]")
     print_numbers_in_range(numbers, 0, 5)
 
 def print_numbers_in_range(numbers, lower_limit, upper_limit):
+    print("The numbers in range [{}, {}]".format(lower_limit, upper_limit))
     for number in numbers:
         if number >= lower_limit and number <= upper_limit:
             print(number)
