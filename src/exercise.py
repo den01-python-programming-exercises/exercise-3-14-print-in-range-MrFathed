69440cf1e75fe4ed7b7144614250a6029d89dcd1
Justin Turner <jmturner638@gmail.com>
2021-08-01 03:24:00 -0500

    Second Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,6 +1,9 @@
 def main():
     #write your code below this line
-    pass
+    numbers = [1,3,10,-1,5,0]
+
+    print("The numbers in the range [0, 5]")
+    print_numbers_in_range(numbers, 0, 5)
 
 def print_numbers_in_range(numbers, lower_limit, upper_limit):
     for number in numbers:
